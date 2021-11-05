87e27dbff0140a7c0ce574aa2edf31fe9b3b3f41
robysetyaw <robysetyaw@gmail.com>
2021-11-05 16:02:37 +0700

    [ADD] Add query option

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -47,23 +47,130 @@ class Item extends Model
     static public function get_transaction(Request $request)
     {
 
-        $status = $request->status;
-        $date_filter_type = $request->date_filter_type; // filter type "this_day', "yesterday", "tomorrow"
+        $status = $request->status ?? NULL;
+
+        $date1 = $request->date1;
+        $date2 = $request->date2;
+        $date_filter_type = $request->date_filter_type; // filter type "this_day', "yesterday", "tomorrow", "beetween"
         
-        if ($date_filter_type === "this_day") {
+        $per_person_id = $request->per_person_id; // customer id, supplier id, gudang id
+        $per_person_type = $request->per_person_type ?? NULL; // filter type "customer", "supplier", "gudang"
+        
+        // this_day with status
+        // query hari ini dengan status in atau out 
+        if ($date_filter_type === "this_day" && $status !== NULL) {
             $db = DB::select("SELECT 
             it.id, it.gudang_id, it.customer_id, it.supplier_id,  it.item_name, it.item_weight, it.item_price ,
             (it.item_price *  it.item_weight) as 'total price', it.status, it.created_at
             FROM items as it 
             WHERE it.status='$status' AND DATE(it.created_at)=CURDATE()");
         }
-        // berdarkan tanggal harini
-        // tomorrow
-        // yesterday
-        // berdasarkan tanggal diantara
-        //  elseif (condition) {
-        //     # code...
-        // }
+
+        // this_day without status
+        // query hari ini tanpa status
+        elseif ($date_filter_type === "this_day") {
+            $db = DB::select("SELECT 
+            it.id, it.gudang_id, it.customer_id, it.supplier_id,  it.item_name, it.item_weight, it.item_price ,
+            (it.item_price *  it.item_weight) as 'total price', it.status, it.created_at
+            FROM items as it 
+            WHERE DATE(it.created_at)=CURDATE()");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // between date with status with per person type
+        // query diantara tanggal date 1 dan date 2 dengan status in atau out dan tipe orang customer
+        elseif($date_filter_type === "between" && $per_person_type !== "customer" && $status !== NULL) {
+            $db = DB::select("SELECT 
+            it.id, it.gudang_id, it.customer_id, it.supplier_id,  it.item_name, it.item_weight, it.item_price ,
+            (it.item_price *  it.item_weight) as 'total price', it.status, it.created_at
+            FROM items as it 
+            WHERE it.status='$status' AND  it.customer_id='$per_person_id'   AND (it.created_at BETWEEN '$date1' AND '$date2') ");
+        }
+
+         // between date with status with per person type
+        // query diantara tanggal date 1 dan date 2 dengan status in atau out dan tipe orang supplier
+        elseif($date_filter_type === "between" && $per_person_type !== "supplier" && $status !== NULL) {
+            $db = DB::select("SELECT 
+            it.id, it.gudang_id, it.customer_id, it.supplier_id,  it.item_name, it.item_weight, it.item_price ,
+            (it.item_price *  it.item_weight) as 'total price', it.status, it.created_at
+            FROM items as it 
+            WHERE it.status='$status' AND  it.supplier_id='$per_person_id'   AND (it.created_at BETWEEN '$date1' AND '$date2') ");
+        }
+
+         // between date with status with per person type
+        // query diantara tanggal date 1 dan date 2 dengan status in atau out dan tipe orang gudang
+        elseif($date_filter_type === "between" && $per_person_type !== "gudang" && $status !== NULL) {
+            $db = DB::select("SELECT 
+            it.id, it.gudang_id, it.customer_id, it.supplier_id,  it.item_name, it.item_weight, it.item_price ,
+            (it.item_price *  it.item_weight) as 'total price', it.status, it.created_at
+            FROM items as it 
+            WHERE it.status='$status' AND  it.gudang_id='$per_person_id'   AND (it.created_at BETWEEN '$date1' AND '$date2') ");
+        }
+
+
+
+
+
+
+
+
+
+
+         // between date without status with per person type
+         // query diantara tanggal date 1 dan date 2, tanpa status,  dan tipe orang customer, 
+        elseif($date_filter_type === "between" && $per_person_type !== "customer") {
+            $db = DB::select("SELECT 
+            it.id, it.gudang_id, it.customer_id, it.supplier_id,  it.item_name, it.item_weight, it.item_price ,
+            (it.item_price *  it.item_weight) as 'total price', it.status, it.created_at
+            FROM items as it 
+            WHERE it.customer_id='$per_person_id' AND  (it.created_at BETWEEN '$date1' AND '$date2') ");
+        }
+
+        // between date without status with per person type
+         // query diantara tanggal date 1 dan date 2, tanpa status,  dan tipe orang supplier, 
+         elseif($date_filter_type === "between" && $per_person_type !== "supplier") {
+            $db = DB::select("SELECT 
+            it.id, it.gudang_id, it.customer_id, it.supplier_id,  it.item_name, it.item_weight, it.item_price ,
+            (it.item_price *  it.item_weight) as 'total price', it.status, it.created_at
+            FROM items as it 
+            WHERE it.supplier_id='$per_person_id' AND  (it.created_at BETWEEN '$date1' AND '$date2') ");
+        }
+        // between date without status with per person type
+         // query diantara tanggal date 1 dan date 2, tanpa status,  dan tipe orang gudang, 
+         elseif($date_filter_type === "between" && $per_person_type !== "gudang") {
+            $db = DB::select("SELECT 
+            it.id, it.gudang_id, it.customer_id, it.supplier_id,  it.item_name, it.item_weight, it.item_price ,
+            (it.item_price *  it.item_weight) as 'total price', it.status, it.created_at
+            FROM items as it 
+            WHERE it.gudang
+             _id='$per_person_id' AND  (it.created_at BETWEEN '$date1' AND '$date2') ");
+        }
+        
+        
+        
+        
+        
+        
+        
+        // all day
+        else {
+            $db = DB::select("SELECT 
+            it.id, it.gudang_id, it.customer_id, it.supplier_id,  it.item_name, it.item_weight, it.item_price ,
+            (it.item_price *  it.item_weight) as 'total price', it.status, it.created_at
+            FROM items as it ");
+        }
+        
 
         return $db;
     }
